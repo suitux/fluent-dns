ff63ee97cf0ffe74c5d05a519fa28edee995c3b3
Xavier Rincon Rico <xavier.rincon@lifullconnect.com>
2021-01-12 18:18:31 +0100

    Force address resolving by searching it on 8.8.8.8 and if not found, searching it with dns nodejs library

--- dns/index.js
+++ dns/index.js
@@ -1,4 +1,7 @@
 const DNS = require('dns2')
+const nodeDns = require('dns');
+const util = require('util')
+const resolve = util.promisify(nodeDns.resolve)
 
 const { Packet } = DNS
 
@@ -11,7 +14,8 @@ const database = new DnsDb({
     dnsEntriesFilePath,
 })
 
-const dnsClient = new DNS()
+const dnsClient = new DNS({nameServers: ['8.8.8.8']})
+
 
 const server = DNS.createServer(async (request, send, rinfo) => {
     const response = Packet.createResponseFromRequest(request)
@@ -31,6 +35,32 @@ const server = DNS.createServer(async (request, send, rinfo) => {
             type: entry.type,
             class: entry.class,
         })
+    } else {
+        const entry = await dnsClient
+            .resolve(
+                question.name,
+                Object.keys(Packet.TYPE)[question.type - 1],
+                question.class
+            )
+            .catch((e) => console.log(e))
+
+        let answers = entry.answers
+
+        if(entry.answers.length === 0) {
+            const res = await resolve(question.name, Object.keys(Packet.TYPE)[question.type - 1]).catch(e => console.log(e));
+
+            if(res && res.length > 0) {
+                answers = [{
+                    name: entry.name,
+                    ttl: 300,
+                    type: question.type,
+                    class: question.class,
+                    address: res[0]
+                }]
+            }
+        }
+
+        response.answers.push(...entry.answers)
     }
 
     send(response)
